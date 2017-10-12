9dc61762b43d30d338ff33942291862811c1f0fd
tyronmaxico2008 <amitjha211@outlook.com>
2017-10-12 20:49:30 +0530

    join.py modified for pygrunt

--- join.py
+++ join.py
@@ -1,8 +1,8 @@
 import sys
 from aj import task
 
-config = {
-    "out" : "d:\\result.js"
+common = {
+    "out" : ".\\..\\jsDemo\\build\\common.js"
     ,"input" : [
         "common\\clsAjaxProcessing.js"
         ,"common\\g.js"
@@ -10,6 +10,39 @@ config = {
         ,"common\\myAjax.js"
     ]
 }
+
+ng = {
+    "out" : ".\\..\\jsDemo\\build\\ngBll.js"
+    ,"input" : [
+        'build\\common.js'
+        ,'ng\\ngCommon.js'
+        ,'ng\\clsAppConfig.js'
+        ,'ng\\bll.js'
+        ,'ng\\ngCRUD.js'
+        ,'ng\\directives\\pager.js'
+        ,'ng\\directives\\sorter.js'
+        ,'ng\\directives\\busy.js'
+        ,'ng\\directives\\grd-filter.js'
+        ,'ng\\directives\\test.js'
+        ,'ng\\directives\\drp.js'
+        ,'ng\\directives\\fld.js'
+        ,'ng\\directives\\file-model.js'
+        ,'ng\\commonControllers\\CRUDControllers.js'
+    ]
+}
+
+vue  = {
+    "out" : ".\\..\\jsDemo\\build\\vueBll.js"
+    ,"input" : [
+        'assets\\admin\\common.js'
+        ,'assets\\admin\\vueUtility\\clsConfig.js'
+        ,'assets\\admin\\vueUtility\\Common.js'
+        ,'assets\\admin\\vueUtility\\Grid.js'
+        ,'assets\\admin\\vueUtility\\clsFilterField.js'
+        ,'assets\\admin\\vueUtility\\components.js'
+    ]
+}
+
 
 
 
